aae232588dbd0eb8bee55869474bccce1fe520e2
Jannine Chamorro <janninechamorro@gmail.com>
2020-08-15 22:12:32 -0700

    Added some text to the methods page.

--- app.R
+++ app.R
@@ -6,6 +6,7 @@ library(shinythemes)
 library(dplyr)
 library(ggplot2)
 library(slickR)
+library(here)
 
 #############################################
 
@@ -35,9 +36,12 @@ ui<-navbarPage("Intertidal temperature data along the Pacific Coast",
                         a("MARINe (Multi-Agency Rocky Intertidal Network)", 
                           href="https://marine.ucsc.edu/index.html"),
                         h6("Photos by Jannine Chamorro.", align = "center")),
-               tabPanel("Methods"),
-               tabPanel("Explore a site"),
-               tabPanel("Compare sites"))
+               tabPanel("Methods", 
+                        h2("How is this data collected?"),
+                        p("Temperature data is collected using biomimetic temperature loggers called Robomussels. Robomussels are different from other temperature loggers because they thermally match living mussel, meaning they are similar in size, color, shape and thermal inertia. Using Robomussels allows us to more accurately measure temperature experienced by mussels." ),
+                        p("Multiple Robomussels are installed at each site, they are placed at different heights in the mussel bed to measure the wide range of temperatures experienced by mussels.")),
+               tabPanel("Explore a Site"),
+               tabPanel("Compare Sites"))
 
 server<-function(input, output) {
   
@@ -51,7 +55,6 @@ server<-function(input, output) {
     )
   })
 
-  
   
 }
   
