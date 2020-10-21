056fb29af8c8dac82d4cd774ed7683fdf971f00c
Cristina Dobbelaere <cdobbelaere@ucsb.edu>
2020-10-21 12:50:13 -0700

    map is now reactive

--- app.R
+++ app.R
@@ -14,8 +14,7 @@ library(leaflet)
 
 setwd("~/Documents/GitHub/Intertidal-Temp-Shiny-App/")
 
-all_sites <- read.csv("~/Documents/GitHub~Intertidal-Temp-Shiny-App/InfoMicrosite_2016_mussel.csv")
-
+all_sites <- read.csv("~/Documents/GitHub/Intertidal-Temp-Shiny-App/InfoMicrosite_2016_mussel.csv")
 
 # Subset West Coast sites
 site_data = filter(all_sites, 
@@ -33,6 +32,7 @@ temp_data<-read_csv(file="temp_data.csv")
 ui<-navbarPage("Intertidal temperature data along the Pacific Coast",
                theme = shinytheme("flatly"),
                
+               #############################################
                # Welcome page
                tabPanel("Home",
                         
@@ -80,7 +80,7 @@ ui<-navbarPage("Intertidal temperature data along the Pacific Coast",
                         ),
                
 
-               
+               #############################################
                # Methods page
                tabPanel("Methods", 
                       tags$div(
@@ -104,22 +104,25 @@ ui<-navbarPage("Intertidal temperature data along the Pacific Coast",
                                     margin-left: auto; 
                                     margin-right: auto;")), #this part of this centers the image
 
-               
+               #############################################
                # Single site page
                tabPanel("Explore a Site",
-                        sidebarLayout(
-                          sidebarPanel("",
-                                        selectInput(
-                                         inputId = "radio_location",
-                                         label = "Select a site!",
-                                         choices = c(levels(unique_site_data$location
-                                         )))),
-                          mainPanel("Main panel text",
-                                    leafletOutput(outputId = "map1",
-                                                  width = "90%",
-                                                  height = "500")
-                                    ))),
+                        sidebarPanel(
+                                      #Select which site to show on map
+                                      selectInput("SiteSelect",
+                                          label = "Choose a site!",
+                                          choices = unique(unique_site_data$location))
+                                      ),
+                                      
+                        mainPanel(
+                                      "",
+                                      leafletOutput(outputId = "map1",
+                                                    width = "60%",
+                                                    height = "400")
+                        )),
                
+               
+               #############################################
                # I made replicate single site page to try an figure out how to plot the temp data. 
                # Once I figure it out we can combine it with the original page.
                tabPanel("Explore a Site",
@@ -140,9 +143,9 @@ ui<-navbarPage("Intertidal temperature data along the Pacific Coast",
                                       sliderInput(
                                             inputId = "dateFinder",
                                             label = "Date",
-                                            value= c(as.Date(min(temp_data$date)), as.Date(max(temp_data$date))),
-                                            min= as.Date(min(temp_data$date)),
-                                            max= as.Date(max(temp_data$date)),
+                                            value= c(as.Date(min(temp_data$local_datetime), format="%Y-%m-%d %h:%m:%s"), as.Date(max(temp_data$local_datetime), format="%Y-%m-%d %h:%m:%s")),
+                                            min= as.Date(min(temp_data$local_datetime), format="%Y-%m-%d %h:%m:%s"),
+                                            max= as.Date(max(temp_data$local_datetime), format="%Y-%m-%d %h:%m:%s"),
                                             timeFormat="%b %Y")), 
                        #Here is some text to determine if my stuff is getting pushed
                         
@@ -154,7 +157,7 @@ ui<-navbarPage("Intertidal temperature data along the Pacific Coast",
 
 
 #############################################
-server<-function(input, output) {
+server<-function(input, output, session) {
   
   # Create welcome page photo slideshow
   output$welcome_slideshow <- renderSlickR({
@@ -168,24 +171,34 @@ server<-function(input, output) {
   })
   
   # Create reactive site dataframe
-  locations <- reactive({
-    unique_site_data %>% 
-      filter(location %in% input$locations) 
-  })
-
+  #locations <- reactive({
+  #  unique_site_data %>% 
+  #    filter(location %in% input$locations) 
+  #})
+  
+  #location_reactive <- reactive({
+  #  if(is.null(input$SiteSelect)){
+  #    unique_site_data
+  #  }else{
+  #    unique_site_data[unique_site_data$location %in% input$SiteSelect, ]
+  #  }
+  #})
 
   # Create map for sites
-  leaflet(options = leafletOptions(minZoom = 4))
+  #leaflet(options = leafletOptions(minZoom = 4))
   
   output$map1 <- renderLeaflet({
-    leaflet(data=unique_site_data) %>% 
-    addProviderTiles(providers$Esri.OceanBasemap,
-                     providerTileOptions(detectRetina = T)) %>% 
+    leaflet( data=unique_site_data ) %>% 
+    addProviderTiles(providers$Esri.WorldPhysical,
+                     providerTileOptions(detectRetina = T)) %>%
+#) %>% 
     addMarkers(
       lng = unique_site_data$field_lon,
       lat = unique_site_data$field_lat,
       label = unique_site_data$location,
+      group = "myMarkers",
       labelOptions = labelOptions(direction = "bottom",
+                                  offset = c(2,2), sticky = T,
                                   style = list(
                                     "color" = "black",
                                     "font-family" = "helvetica",
@@ -194,7 +207,8 @@ server<-function(input, output) {
                                     "font-size" = "12px",
                                     "border-color" = "rgba(0,0,0,0.5)"
                                   )),
-      options = markerOptions(interactive = T, clickable = NULL),
+      options = markerOptions(interactive = F, clickable = T, riseOnHover = F),
+    #  #clusterOptions = markerClusterOptions(),
       popup = paste(unique_site_data$location, ", ", 
                     unique_site_data$state_province, "<br>",
                     "(", unique_site_data$field_lat, "ºN, ", 
@@ -203,14 +217,74 @@ server<-function(input, output) {
                                   minWidth = 50, maxHeight = NULL,
                                   autoPan = T, keepInView = F, closeButton = T)
     ) %>% 
-    addMiniMap(
-      tiles = providers$Esri.OceanBasemap,
-      toggleDisplay = T
-    ) %>% 
+    setView(lat = 42,	lng = -130, zoom = 4) %>% 
+    #addMiniMap(
+    #  tiles = providers$Esri.OceanBasemap,
+    #  toggleDisplay = T
+    #) %>% 
     addEasyButton(easyButton(
       icon="fa-globe", title="Zoom out to level 1",
       onClick=JS("function(btn, map){ map.setZoom(1); }")))
   })
+  
+  observeEvent(input$SiteSelect, {
+    leafletProxy("map1") %>% 
+      clearGroup("myMarkers") %>% 
+      addMarkers(
+        data = unique_site_data[unique_site_data$location == input$SiteSelect, ],
+        lng = ~field_lon,
+        lat = ~field_lat,
+        label = ~location,
+        group = "myMarkers",
+        labelOptions = labelOptions(direction = "left",
+                                    permanent = T,
+                                    style = list(
+                                      "color" = "black",
+                                      "font-family" = "helvetica",
+                                      "font-style" = "italic",
+                                      "box-shadow" = "3px 3px rgba(0,0,0,0.25)",
+                                      "font-size" = "12px",
+                                      "border-color" = "rgba(0,0,0,0.5)"
+                                    ))) #%>% 
+      #flyTo(#unique_site_data[unique_site_data$location == input$SiteSelect, ],
+      #      lng = ~field_lon, lat = ~field_lat, 
+        #lng = ~unique_site_data$field_lon[unique_site_data$location == input$SiteSelect, ],
+        #lat = ~unique_site_data$field_lat[unique_site_data$location == input$SiteSelect, ],
+      #  zoom = 10)
+    
+      
+        #popup = paste(
+        #  unique_site_data$location[unique_site_data$location == input$SiteSelect, ]
+        #),
+        #popupOptions = popupOptions(maxWidth = 300, 
+        #                            minWidth = 50, maxHeight = NULL,
+        #                            autoPan = T, keepInView = F, closeButton = T))
+      
+      
+    
+    #if (is.null(input$SiteSelect)){
+    #  unique_site_data = unique_site_data
+    #}else{
+    #  unique_site_data = unique_site_data[unique_site_data$location %in% input$SiteSelect,]
+    #}
+    #leafletProxy("map1") %>% 
+    #  clearMarkers() %>% 
+    #  addMarkers(
+    #    data = location_rea,
+    #    lng = ~field_lon,
+    #    lat = ~field_lat,
+    #    label = ~location,
+    #  )
+  }
+)
+    
+    
+    
+    
+    #input$map1_marker_click, {
+    #leafletProxy("map1", session) %>% 
+    #  removeMarker(input$map1_marker_click$id)
+#  })
 
 ### Explore a Site Page
       
